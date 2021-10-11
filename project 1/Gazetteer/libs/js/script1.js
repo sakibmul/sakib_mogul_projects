a496e76c7d81bc462f5a47def6c15d155243d509
Sakib Mogul <zakmog@outlook.com>
2021-10-11 11:25:31 +0100

    Add files via upload

---
+++ project 1/Gazetteer/libs/js/script1.js
@@ -0,0 +1,423 @@
+ // Main Map	
+
+ let myMap = L.map('map');
+    
+ let attribution = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+
+ let tileUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
+
+ let tiles = L.tileLayer(tileUrl, {attribution});
+
+ tiles.addTo(myMap);
+
+// Button options L.easyButton plugin
+let countryInfo = L.easyButton('<strong>i<strong>', function(){
+$('#myModal').modal('show');
+}).addTo(myMap);  
+
+let financialInfo = L.easyButton('fas fa-pound-sign', function(){
+$('#myModal1').modal('show');
+}).addTo(myMap);
+
+let weatherInfo = L.easyButton('fas fa-cloud-sun', function(){
+$('#myModal2').modal('show');
+}).addTo(myMap);
+
+let emissionInfo = L.easyButton('far fa-newspaper', function(){
+$('#myModal3').modal('show');
+}).addTo(myMap);
+
+let wikiInfo = L.easyButton('fab fa-wikipedia-w', function(){
+$('#myModal4').modal('show');
+}).addTo(myMap);
+
+let userPoint = L.layerGroup().addTo(myMap);
+let markerCluster = L.markerClusterGroup().addTo(myMap);
+
+let wikiMarker = L.AwesomeMarkers.icon({
+   icon: 'star',
+   markerColor: 'purple',
+   prefix: 'fa'
+});
+
+let cityMarker = L.AwesomeMarkers.icon({
+   icon: 'fas fa-city',
+   markerColor: 'red',
+   prefix: 'fa'
+});
+
+// Knowing your location
+if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(success);
+
+function success(pos) {
+   console.log(pos.coords.latitude);
+   console.log(pos.coords.longitude);
+
+   let latitude = pos.coords.latitude;
+   let longitude = pos.coords.longitude;
+   
+   let home = L.AwesomeMarkers.icon({
+      icon: 'home',
+      prefix: 'fa'
+   });
+   L.marker([latitude, longitude], {icon: home}).addTo(userPoint).bindPopup('You are here!');
+   myMap.locate({setView: true, maxZoom: 11});
+
+   // ajax call to come within the success function. Call to trigger Country Drop down on load
+$.ajax({
+   url: "libs/php/getCountryName.php",
+   type: "GET",
+   dataType: "json",
+   data: {
+      lat: pos.coords.latitude,
+      lng: pos.coords.longitude
+   },
+   success: function(result) {
+      console.log(result);
+      $('#countryDropdown').val(result['data']['countryCode']).change();
+      
+   },
+   error: function (jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+      console.log(textStatus);
+      console.log(jqXHR);
+   }
+});// End of Country Drop down trigger 
+function error (err) {
+   console.log(err);
+}
+
+// Call for user current user weather and forecast.
+$.ajax({
+   url: "libs/php/getLocWeather.php",
+   type: "GET",
+   dataType: "json",
+   data: {
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+   },
+   success: function(userResult) {
+      console.log(userResult);
+      
+      $('#userTemp').html('Current Location Temperature: ' + userResult['data']['current']['temp'] + ' Celsius');
+      $('#userDes').html('Current Weather Description: ' + userResult['data']['current']['weather'][0]['description']);
+      $('#userLoc').html('The Users Location Weather Symbol is: ')  + $('#userWeatherIcon').attr('src', 'https://openweathermap.org/img/wn/' + userResult['data']['current']['weather'][0]['icon'] + '@2x.png');
+},
+   error: function (jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+      console.log(textStatus);
+      console.log(jqXHR);
+   }
+});// End of User Weather Location call
+
+// Call for Wikipedia Entries Nearby
+$.ajax({
+   url: "libs/php/getWikiEntriesInfo.php",
+   type: "GET",
+   dataType: "json",
+   data: {
+      lat: pos.coords.latitude,
+      lng: pos.coords.longitude
+   },
+   success: function(result) {
+      console.log(result);
+      
+      for (let i = 0; i < result.data.geonames.length; i++) {
+      L.marker([result['data']['geonames'][i]['lat'],
+      result['data']['geonames'][i]['lng']], {icon: wikiMarker})
+      .addTo(markerCluster).bindPopup('<a href="https://' + result['data']['geonames'][i]['wikipediaUrl'] + '">' + result['data']['geonames'][i]['wikipediaUrl'] + '</a>');
+      };
+      alert('Click OK to continue. Then click on a Marker!');
+   },
+   error: function (jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+      console.log(textStatus);
+      console.log(jqXHR);
+   }
+}); // End of Wiki Entries Nearby
+
+}// End of function success call from Latitude and Longitude
+}// End of if statement geolocation call to gain Lat and Long
+
+// Populating the country select
+$.ajax({
+url: "libs/php/getJsonData.php",
+type: 'GET', 
+dataType: 'json',
+success: function (dropResult) {
+   console.log(dropResult);
+   
+   $.each(dropResult.data, function(index) {
+      $('#countryDropdown').append($('<option>', {
+      text: dropResult.data[index].name,
+      value: dropResult.data[index].isoCode
+   }));
+   });
+},
+error: function (jqXHR, textStatus, errorThrown) {
+   console.log(errorThrown);
+   console.log(textStatus);
+   console.log(jqXHR);
+}
+});
+
+// Country select feature, highlights and flies to the country!
+$('#countryDropdown').change(function() {
+
+   $.ajax({
+     url: "libs/php/getJsonGeometry.php",
+     type: "GET",
+     dataType: "json",
+   data: {
+      iso_code: $("#countryDropdown").val(),
+   },
+     success: function (flyResult) {
+      console.log(flyResult);
+      $('#countryDropdown').change(function() {
+         if (border) {
+            border.remove(myMap);
+         } 
+
+         if (wikiMarker, cityMarker) {
+            myMap.removeLayer(wikiMarker, cityMarker);
+         }
+      });
+
+         let border = L.geoJSON(flyResult["data"], {
+             style: function () {
+                 return {
+                     color: '#ba421e'
+                 };
+             },
+         }).addTo(myMap);
+         console.log("border: ", border);
+         myMap.fitBounds(border.getBounds());
+   },
+   error: function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+   },
+});
+
+// Country info call
+$.ajax({
+   url: "libs/php/getCountryInfo.php",
+   type: "POST",
+   dataType: "json",
+   data: {
+         country: $('#countryDropdown').val()
+   },
+   success: function(infoResult) {
+      console.log(infoResult);
+
+      if (infoResult.status.name == "ok")  {
+
+         $('.modal-title').html(infoResult['data'][0]['countryName']);
+         $('#continent').html(infoResult['data'][0]['continentName']);
+         $('#capital').html(infoResult['data'][0]['capital']);
+         $('#language').html(infoResult['data'][0]['languages']);
+         $('#population').html(infoResult['data'][0]['population']);
+         $('#area').html(infoResult['data'][0]['areaInSqKm']);	
+         	
+      // Wikipedia button function.
+            let countrySplit = infoResult['data'][0]['countryName'].split(" ").join("%20");
+            $.ajax({
+               url: "libs/php/getWikiInfo.php",
+               type: "POST",
+               dataType: "json",
+               data: {
+                     country: countrySplit,
+               },
+               success: function(wikiResult) {
+                  console.log(wikiResult);
+                  $('#wikiSumm').html(wikiResult['data']['geonames'][0]['summary']);
+                  $('#wikiUrl').html('<a href="https://' + wikiResult['data']['geonames'][0]['wikipediaUrl'] + '">'  + wikiResult['data']['geonames'][0]['wikipediaUrl'] + '</a>');
+               },
+                  error: function(jqXHR, textStatus, errorThrown) {
+                  console.log(jqXHR);
+                  console.log(textStatus);
+                  console.log(errorThrown);
+               },
+            });
+      
+      // Weather info call for Capital city
+      $.ajax({
+         url: "libs/php/getWeatherInfo.php",
+         type: "POST",
+         dataType: "json",
+         data: {
+               capital: infoResult['data'][0]['capital'],
+         },
+         success: function(weatherResult) {
+            console.log(weatherResult);
+      
+            $('#capTemp').html('Current Temperature in ' + infoResult['data'][0]['capital'] + ' is:').append(' ' + weatherResult['data']['list'][0]['main']['temp'] + ' Celsius');
+            $('#weatherMain').html('The Main Weather Description in ' + infoResult['data'][0]['capital'] + ' is:').append(' ' + weatherResult['data']['list'][0]['weather'][0]['main']);
+            $('#weatherDes').html('Weather Description in ' + infoResult['data'][0]['capital'] + ' is:').append(' ' + weatherResult['data']['list'][0]['weather'][0]['description']);
+            $('#windSpeed').html('The Wind Speed in ' + infoResult['data'][0]['capital'] + ' is:').append(' ' + weatherResult['data']['list'][0]['wind']['speed'] + ' m/s');
+            $('#capName').html('The Weather Symbol for ' + infoResult['data'][0]['capital'] + ' today is:') + $('#capWeatherIcon').attr('src', 'https://openweathermap.org/img/wn/' + weatherResult['data']['list'][0]['weather'][0]['icon'] + '@2x.png');
+      },
+            error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+         },
+      });// End of weather info call
+      
+      $.ajax({
+         url: "libs/php/getCountryNews.php",
+         type: "POST",
+         dataType: "json",
+         data: {
+               code: $('#countryDropdown').val(),
+         },
+         success: function(newsResult) {
+            console.log(newsResult);
+      
+            $('#newsHead').html(newsResult['data']['articles'][0]['title']);
+            $('#news').html('<a href="https://' + newsResult['data']['articles'][0]['url'] + '">'  + newsResult['data']['articles'][0]['url'] + '</a>');
+
+            $('#newsHead1').html(newsResult['data']['articles'][1]['title']);
+            $('#news1').html('<a href="https://' + newsResult['data']['articles'][1]['url'] + '">'  + newsResult['data']['articles'][1]['url'] + '</a>');
+
+            $('#newsHead2').html(newsResult['data']['articles'][2]['title']);
+            $('#news2').html('<a href="https://' + newsResult['data']['articles'][2]['url'] + '">'  + newsResult['data']['articles'][2]['url'] + '</a>');
+
+            $('#newsHead3').html(newsResult['data']['articles'][3]['title']);
+            $('#news3').html('<a href="https://' + newsResult['data']['articles'][3]['url'] + '">'  + newsResult['data']['articles'][3]['url'] + '</a>');
+
+            $('#newsHead4').html(newsResult['data']['articles'][4]['title']);
+            $('#news4').html('<a href="https://' + newsResult['data']['articles'][4]['url'] + '">'  + newsResult['data']['articles'][4]['url'] + '</a>');
+            
+         },
+            error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+         },
+      });// End of news api call 
+
+      
+
+}
+   },
+      error: function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+   },
+});// End of Country Info call
+
+// Rest Country call
+$.ajax({
+   url: "libs/php/getRestCountryData.php",
+   type: 'POST',
+   dataType: 'json',
+   data:{
+         code: $("#countryDropdown").val(),
+   },
+   
+      success: function(restResult){
+         console.log(restResult); 
+
+         if (restResult.status.name == 'ok') {
+	
+            $('#currencyCode').html(restResult['data']['currencies'][0]['code']);
+            $('#currencyName').html(restResult['data']['currencies'][0]['name']);
+            $('#currencySymbol').html(restResult['data']['currencies'][0]['symbol']);
+            $('.flag').attr('src', restResult['data']['flags']['svg']);
+
+            $.ajax({
+               url: "libs/php/getJsonExRate.php",
+               type: "POST",
+               dataType: "json",
+               data: {
+                     currencyCode: restResult['data']['currencies'][0]['code'],
+               },
+               success: function(currResult) {
+                   console.log(currResult);
+                  
+                   $('#exRate').html(currResult['data']['rates'][restResult['data']['currencies'][0]['code']]).append(' To 1 US Dollar.');
+               },
+            
+               error: function(jqXHR, textStatus, errorThrown) {
+                  console.log(jqXHR);
+                  console.log(textStatus);
+                  console.log(errorThrown);
+               }
+            });// End of Exchange Rate ajax call
+         }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+   },
+});// End of Rest Country call
+
+ // Cities API Call and adding Markers
+ $.ajax({
+   url: "libs/php/getCitiesMarker.php",
+   type: "GET",
+   dataType: "json",
+   data: {
+         country: $('#countryDropdown').val(),
+   },
+   success: function(cityResult) {
+   console.log(cityResult);
+
+   for (let c = 0; c < cityResult.data.geonames.length; c++) {
+      L.marker([cityResult['data']['geonames'][c]['lat'],
+      cityResult['data']['geonames'][c]['lng']], {icon: cityMarker})
+      .on('click', markerClick).addTo(markerCluster);
+
+      function markerClick() {
+      
+         $('#myModal5').modal('show');
+         $('.modal-title1').html(cityResult['data']['geonames'][c]['name']);
+         $('#cityPop').html('The City Center Population is: ' + cityResult['data']['geonames'][c]['population']);
+         
+         $.ajax({
+            url: "libs/php/getLocWeather.php",
+            type: "GET",
+            dataType: "json",
+            data: {
+               lat: cityResult['data']['geonames'][c]['lat'],
+               lon: cityResult['data']['geonames'][c]['lng']
+            },
+            success: function(result) {
+               console.log(result);
+
+               $('#cityTemp').html('The current temperature for ' + cityResult['data']['geonames'][c]['name'] + ' is ').append(result['data']['current']['temp'] + ' Celsius');
+               $('#cityDes').html('The weather description for  ' + cityResult['data']['geonames'][c]['name'] + ' is ').append(result['data']['current']['weather'][0]['description']);
+               $('#cityLoc').html('The City`s Weather Symbol is: ')  + $('#cityWeatherIcon').attr('src', 'https://openweathermap.org/img/wn/' + result['data']['current']['weather'][0]['icon'] + '@2x.png');
+            }, 
+            error: function (jqXHR, textStatus, errorThrown) {
+               console.log(errorThrown);
+               console.log(textStatus);
+               console.log(jqXHR);
+            }
+         });
+
+      };
+
+   };
+
+},
+error: function (jqXHR, textStatus, errorThrown) {
+   console.log(errorThrown);
+   console.log(textStatus);
+   console.log(jqXHR);
+}
+});// End of city marker call
+
+// Clearing all Markers on Country change
+markerCluster.clearLayers();
+
+
+
+
+});// On change function end
+
